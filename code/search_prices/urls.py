2219fb97f72d0218ef31d3e9cd09026ff2b959de
PC Roma <joaoricardoroma@gmail.com>
2021-05-11 19:51:15 -0300

    "tela de cadastro de cliente, tela de perfil e deletar perfil"

--- code/search_prices/urls.py
+++ code/search_prices/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'search_prices'
 urlpatterns = [
     path('', views.search, name='search'),
-    path('delete', views.delete_search, name='delete_search'),
+    path('client_register', views.register_client, name='register_client'),
+    path('<int:pk>/client_profile', views.client_profile, name='client_profile'),
+    path('<int:pk>/delete', views.delete_search, name='delete_search'),
+    path('<int:pk>/client_profile/delete', views.delete_client, name='delete_client'),
     ]
